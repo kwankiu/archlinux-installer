bf2db9f5b5c51d6318e2d610e632dfa1ca06cee9
Samuel <ngkwankiuu@gmail.com>
2023-08-07 06:22:21 -0700

    Fixing and improving the script, automatically get boot image, etc

--- arch-rock5b.sh
+++ arch-rock5b.sh
@@ -1,47 +1,124 @@
 #!/bin/bash
 
+################################################################
+#                                                              #
+#  Arch Linux image / disk creation tool for Rock 5B / RK3588  #
+#                                                              #
+################################################################
+
+# Function to display a list of available disks
+list_disks() {
+  echo "Available disks:"
+  disks=($(lsblk -rdno NAME,SIZE,MODEL))
+  for ((i=0; i<${#disks[@]}; i+=3)); do
+    model=${disks[i+2]//\\x20/ }  # Replace escaped spaces with actual spaces
+    echo "$((i/3+1))) /dev/${disks[i]} - $model (${disks[i+1]})"
+  done
+  echo "$((i/3+1))) Create an (.img) image"
+  echo "$((i/3+2))) Enter disk path manually"
+}
+
+# Function to prompt user for disk selection
+select_disk() {
+  read -p "Select a disk or enter path: " choice
+  if [[ $choice =~ ^[0-9]+$ && $choice -ge 1 && $choice -le $((i/3+2)) ]]; then
+    if [[ $choice -le $((i/3)) ]]; then
+      selected_disk="/dev/${disks[((choice-1)*3)]}"
+      echo "Selected disk $selected_disk."
+    elif [[ $choice -eq $((i/3+1)) ]]; then
+      selected_disk="/out/archlinux.img"
+      echo "Creating an (.img) image at $selected_disk."
+    else
+      read -p "Enter the disk path: " selected_disk
+      echo "Manually entered disk path $selected_disk."
+    fi
+  else
+    echo "Invalid choice. Please select a valid option."
+    select_disk
+  fi
+}
+
+# Sees if it is a help command or normal command with parameters
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+  echo "Usage : To be written"
+  exit 1
+else
+  echo "---------------------------------------------------------------------"
+  echo "Welcome to Arch Linux image / disk creation tool for Rock 5B / RK3588"
+  echo "---------------------------------------------------------------------"
+  drive=$1
+fi
+
 # Check if parted is installed
 if ! [ -x "$(command -v parted)" ]; then
   # Check Linux distribution
-  if [ -f /etc/lsb-release ]; then
-    # Ubuntu-based
+  echo "Parted is not found, trying to install..."
+  if [ -f /etc/lsb-release ] || [ -x "$(command -v apt-get)" ]; then
+    # Debian/Ubuntu-based
     sudo apt-get update
-    sudo apt-get install -y parted
+    sudo apt-get install -y parted libarchive-tools
   elif [ -f /etc/redhat-release ]; then
     # Red Hat-based
     sudo yum update
-    sudo yum install -y parted
+    sudo yum install -y parted libarchive-tools
   elif [ -f /etc/arch-release ]; then
     # Arch Linux
-    sudo pacman -S parted
+    sudo pacman -S parted --noconfirm
+  elif [ -x "$(command -v apk)" ]; then
+    # Alphine-based
+    apk add parted libarchive-tools
   else
-    echo "This script only supports Ubuntu, Red Hat, and Arch Linux distributions."
+    echo "Error : We cant find or install parted on your system. Exiting..."
     exit 1
   fi
+
+# Verify if it is installed
+echo "---------------------------------------------------------------------"
+if [ -x "$(command -v parted)" ]; then
+  echo "Package installed, continuing..."
+  echo "---------------------------------------------------------------------"
+else
+  echo "Error : Package not installed, please check your package manager / internet connection / system. Exiting..."
+  exit 1
+fi
 fi
 
-drive=$1
+# This runs when there is no parameters specified
+# Choose drive to install / create .img image
 if [ -z $drive ]; then
-  echo "Do you want to create an image (y/n)?"
-  read answer
-  if [ "$answer" == "y" ]; then
+  echo "Choose drive to install or create an image:"
+  list_disks
+  select_disk
+  if [ $selected_disk == "/out/archlinux.img" ]; then
     if [ ! -d "./out" ]; then
       sudo mkdir ./out
     fi
     drive=./out/archlinux.img
     sudo dd if=/dev/zero of=$drive bs=1M count=4096
   else
-    echo "Please specify a drive (e.g. /dev/sda)"
-    exit 1
+    drive=$selected_disk
+    echo "Are you sure to set up disk $selected_disk (WARNING : all data of the disk will be deleted) ? (y/n)"
+    read answer
+    if [ $answer = "n" ]; then
+      echo "Aborted. Exiting ..."
+      exit 1
+    fi
   fi
 fi
 
+# boot partition image if not specified
 boot_image=$2
+
 if [ -z $boot_image ]; then
-  if [ -z $drive ]; then
-    echo "Specify a boot image (e.g. /path/to/boot.tar.gz or /path/to/boot.img): "
+  if [ -z $2 ]; then
+    echo "Do you want to download the required boot partition image? (Leave empty or enter "y" to download automatically or Enter the path if you have your own boot image instead (e.g. /path/to/boot.tar.gz or /path/to/boot.img)"
     read answer
-    boot_image=answer
+    if [ "$answer" = "y" ] || [ "$answer" = "yes" ] || [ "$answer" = "" ] || [ ! "$answer" ]; then
+      curl -LJO https://github.com/kwankiu/archlinux-installer-rock5/releases/download/latest/boot-arch-rkbsp-latest.tar.gz
+      boot_image="boot-arch-rkbsp-latest.tar.gz"
+    else
+      boot_image=answer
+    fi
    else
     echo "Please specify a boot image (e.g. /path/to/boot.tar.gz or /path/to/boot.img)"
     exit 1
@@ -106,12 +183,16 @@ fi
 root_uuid=$(sudo blkid $root_partition | awk '{print $2}' | tr -d '"')
 root_part_uuid=$(sudo blkid -o export $root_partition | grep PARTUUID | awk -F= '{print $2}')
 
+echo "Root partition UUID: $root_uuid"
+echo "Root partition PARTUUID: $root_part_uuid"
+
+# Change UUID for extlinux.conf
+
+
 # Unmount the boot and root partitions
 sudo umount $boot_mount_dir $root_mount_dir
 
 # Clean up
-rm -rf $boot_mount_dir $root_mount_dir ArchLinuxARM-aarch64-latest.tar.gz
+sudo rm -rf $boot_mount_dir $root_mount_dir ArchLinuxARM-aarch64-latest.tar.gz boot-arch-rkbsp-latest.tar.gz
 
 echo "Process completed successfully"
-echo "Root partition UUID: $root_uuid"
-echo "Root partition PARTUUID: $root_part_uuid"
