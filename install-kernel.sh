7b92b39baee8977977b6de882792ae45478272c5
Samuel <ngkwankiuu@gmail.com>
2023-08-09 03:12:26 -0700

    Update tools scripts
    install-kernel.sh
    Update extlinux.conf for midstream.
    
    post-install.sh
    Fix permssion denied
    Add install bluez
    Add support to install mesa

--- install-kernel.sh
+++ install-kernel.sh
@@ -9,40 +9,71 @@
 kernel_repo_dir="kernel-repo-dir"
 
 install_rkbsp5_bin() {
-    echo "Installing linux-radxa-rkbsp5-bin ..."
+    echo "Downloading linux-radxa-rkbsp5-bin ..."
     git clone https://aur.archlinux.org/linux-radxa-rkbsp5-bin.git
     cd linux-radxa-rkbsp5-bin
     makepkg -si
     cd ..
+    echo "Installed linux-radxa-rkbsp5"
 }
 
 install_rkbsp5_git() {
-    echo "Installing linux-radxa-rkbsp5-git ..."
+    echo "Downloading linux-radxa-rkbsp5-git ..."
     git clone https://aur.archlinux.org/linux-radxa-rkbsp5-git.git
     cd linux-radxa-rkbsp5-git
     makepkg -si
     cd ..
+    echo "Installed linux-radxa-rkbsp5"
 }
 
 install_midstream() {    
-    echo "Installing linux-rk3588-midstream ..."
+    echo "Downloading linux-rk3588-midstream ..."
     git clone https://github.com/hbiyik/hw_necromancer.git
     cd hw_necromancer/rock5b/linux-rk3588-midstream
     makepkg -si
     cd ..
     cd ..
     cd ..
+    echo "Installed linux-rk3588-midstream"
+
+    # apply new extlinux.conf
+    sudo mv /boot/extlinux/extlinux.conf /boot/extlinux/arch.extlinux.old
+    sudo mv /boot/extlinux/extlinux.arch.template /boot/extlinux/extlinux.conf
+
+    # Find the UUIDs of the root partition
+    root_uuid=$(sudo blkid $root_partition | awk '{print $2}' | tr -d '"')
+    echo "Root partition UUID: $root_uuid"
+
+    # Change UUID for extlinux.conf
+    echo "Updating UUID for extlinux.conf ..."
+    sudo sed -i "s|UUID=\\*\\*CHANGEME\\*\\*|$root_uuid|" /boot/extlinux/extlinux.conf
+    sudo sed -i "s|UUID=CHANGEME|$root_uuid|" /boot/extlinux/extlinux.conf
+
+    # Removing old RKBSP (WIP)
+    # echo "Do you want to remove old rkbsp kernel files (y/n)?"
+    # read answer
+
+    # Remove libmali
+    sudo rm -rf /usr/lib/libmali
+    sudo rm -rf /usr/bin/libmali
+    sudo rm -rf /usr/bin/libmaliw
+
+    # Install / Replace mali_csffw.bin
+    sudo pacman -Sy wget --noconfirm
+    sudo wget -P /lib/firmware https://github.com/JeffyCN/mirrors/raw/488f49467f5b4adb8ae944221698e9a4f9acb0ed/firmware/g610/mali_csffw.bin
+
 }
 
 # Display options to the user
 echo "---------------------------------------------------------------------"
 echo "Install / Reinstall Linux kernel for Rock 5B / RK3588"
 echo "---------------------------------------------------------------------"
+echo ""
 echo "Select a kernel package to install:"
 echo "1. linux-radxa-rkbsp5-bin - Install Radxa BSP Kernel (recommended, but may not be up-to-date)"
 echo "2. linux-radxa-rkbsp5-git - Install Radxa BSP Kernel from source (more up-to-date, but take a few hours)" 
 echo "3. linux-rk3588-midstream - Install Googulator's Midstream kernel (Linux 6.2, but not everything works)"
-
+echo ""
 # Get choice
 read -p "Enter the number of your choice: " choice
 
--- post-install.sh
+++ post-install.sh
@@ -11,17 +11,16 @@ echo "Arch Linux Post Install for Rock 5B / RK3588"
 echo "---------------------------------------------------------------------"
 echo "Starting post installation ..."
 
-# TODO: Add support for pwm fan control?
-
-echo "Fixing bluetooth ..."
 # The fix for some Bluetooth Modules (A8, AX210, etc.)
-sudo echo "blacklist pgdrv" >> /etc/modprobe.d/blacklist.conf
-sudo echo "blacklist btusb" >> /etc/modprobe.d/blacklist.conf
-sudo echo "blacklist btrtl" >> /etc/modprobe.d/blacklist.conf
-sudo echo "blacklist btbcm" >> /etc/modprobe.d/blacklist.conf
-sudo echo "#blacklist btintel" >> /etc/modprobe.d/blacklist.conf
+echo "Applying bluetooth fix for some Bluetooth Modules (A8, AX210, etc.) ..."
+echo "blacklist pgdrv" >> sudo /etc/modprobe.d/blacklist.conf
+echo "blacklist btusb" >> sudo /etc/modprobe.d/blacklist.conf
+echo "blacklist btrtl" >> sudo /etc/modprobe.d/blacklist.conf
+echo "blacklist btbcm" >> sudo /etc/modprobe.d/blacklist.conf
+echo "#blacklist btintel" >> sudo /etc/modprobe.d/blacklist.conf
 
 #For AX210 Wifi and BT to Work
+sudo pacman -Sy wget --noconfirm
 echo "Installing WiFi driver for AX210 ..."
 sudo wget -P /lib/firmware https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/iwlwifi-ty-a0-gf-a0-59.ucode
 sudo mv /lib/firmware/iwlwifi-ty-a0-gf-a0.pnvm /lib/firmware/iwlwifi-ty-a0-gf-a0.pnvm.bak
@@ -30,8 +29,8 @@ echo "Installing Bluetooth driver for AX210 ..."
 sudo wget -P /lib/firmware/intel https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/intel/ibt-0041-0041.sfi
 sudo wget -P /lib/firmware/intel https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/intel/ibt-0041-0041.ddc
 
-# Enable Bluetooth
-sudo systemctl start bluetooth
+# Install Bluetooth
+sudo pacman -Sy bluez
 
 # RK3588 Profile
 # Define the alias lines
@@ -48,41 +47,118 @@ echo "$powersave_alias" >> ~/.bash_aliases
 source ~/.bash_aliases
 echo "SoC Performance Profile Added. You may change your SoC Performance Profile by running performance, ondemand or powersave."
 
+# TODO: Add support for pwm fan control?
+# echo "Do you want to install and enable pwm fan control?"
+
+# Install Mesa and Desktop Environment
 echo "---------------------------------------------------------------------"
 echo "Install desktop environment"
 echo "---------------------------------------------------------------------"
-
+echo ""
 echo "Select a desktop environment to install :"
 echo "1. Gnome"
-echo "2. KDE Plasma" 
-echo "3. Budgie"
-echo "4. XFCE"
-echo "5. LXQt"
-echo "6. Cinnamon"
-echo "7. Cutefish"
-echo "8. Deepin"
-echo "9. MATE"
-echo "10. Sway"
-echo "11. Install GPU acceleration only"
 
+# TODO
+#echo "2. KDE Plasma" 
+#echo "3. Budgie"
+#echo "4. XFCE"
+#echo "5. LXQt"
+#echo "6. Cinnamon"
+#echo "7. Cutefish"
+#echo "8. Deepin"
+#echo "9. MATE"
+#echo "10. Sway"
+
+echo "2. Install GPU acceleration only"
+echo ""
+echo "Pick an option to install :"
+read de_options
+
+echo "---------------------------------------------------------------------"
+echo "Install Mesa / GPU acceleration"
+echo "---------------------------------------------------------------------"
+echo ""
+echo "Select a package to install :"
+echo "1. mesa-panfork-git - for Radxa BSP Kernel (rkbsp5) (linux 5.10.x)"
+echo "2. mesa-pancsf-git - for Googulator's Midstream kernel (linux-rk3588-midstream) (linux 6.2.x)"
+echo ""
 echo "Pick an option to install :"
 read answer
-echo "This script is WIP"
 
+# tmp dir folder name
+tmp_repo_dir="tmp-repo-dir"
+
+# Install required package
+sudo pacman -S git --noconfirm
+sudo pacman -S --needed base-devel --noconfirm
+
+# create and cd to a directory
+cd ~/
+mkdir $tmp_repo_dir
+cd $tmp_repo_dir
+
+if [ "$answer" = 1 ]; then
+    #Install mesa-panfork-git
+    echo "Downloading mesa-panfork-git ..."
+    git clone https://aur.archlinux.org/mesa-panfork-git.git
+    cd mesa-panfork-git
+    makepkg -si
+    cd ..
+    echo "Installed mesa-panfork-git"
+
+elif [ "$answer" = 2 ]; then
+    #Install mesa-pancsf-git
+    echo "Downloading mesa-pancsf-git ..."
+    git clone https://github.com/hbiyik/hw_necromancer.git
+    cd hw_necromancer/rock5b/mesa-pancsf-git
+    makepkg -si
+    cd ..
+    cd ..
+    cd ..
+    echo "Installed mesa-pancsf-git"
+else
+    echo "invalid option, exiting .."
+    sudo rm -rf ~/$tmp_repo_dir
+    exit 1
+fi
+
+# Remove temp dir folder
+echo "Installed successfully. Cleaning up installation files ..."
+sudo rm -rf ~/$tmp_repo_dir
+
+# Install desktop environment
+if [ "$de_options" = 1 ]; then
+    # Install Gnome
+    sudo pacman -Sy gnome
+    sudo systemctl enable gdm
+fi
 
 echo "---------------------------------------------------------------------"
 echo "Install additional packages"
 echo "---------------------------------------------------------------------"
+echo ""
 echo "Select a package to install :"
 echo "1. Browser / Media"
 echo "2. Gaming / Virtualization" 
 echo "3. Misc / Tools"
 echo "4. Others"
-
+echo ""
 echo "Pick an option (Enter 'done' when you finish) :"
-read answer
-echo "This script is WIP"
+#read answer
+echo "This feature is not implemented / WIP, skipping ..."
 
 echo "Setting up Video Decoder Accelaration with FFMpeg & RKMPP ..."
-echo "Setting up Browser Acceleration"
+echo "This feature is not implemented / WIP, skipping ..."
 
+echo "Setting up Browser Acceleration"
+echo "This feature is not implemented / WIP, skipping ..."
+
+# Prompt user if they want to reboot
+read -t 5 -p "Changes have been made. We will reboot your system in 5 seconds. Do you want to reboot now? (y/n): " reboot_choice
+
+if [[ "$reboot_choice" == "n" || "$reboot_choice" == "N" ]]; then
+    echo "You can manually reboot later to apply the changes."
+else
+    echo "Done. Rebooting..."
+    sudo reboot
+fi
