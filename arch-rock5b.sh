cb8c1c9eefaed4ef941e7b4786cc8137fc038d47
Samuel <ngkwankiuu@gmail.com>
2023-08-11 12:55:39 -0700

    set to dev for testing

--- arch-rock5b.sh
+++ arch-rock5b.sh
@@ -8,4 +8,4 @@
 # This install script has been renamed to archlinux-installer
 
 echo "Getting the latest installation script ..."
-bash <(curl -fsSL https://raw.githubusercontent.com/kwankiu/archlinux-installer-rock5/main/archlinux-installer)
+bash <(curl -fsSL https://raw.githubusercontent.com/kwankiu/archlinux-installer-rock5/dev/archlinux-installer)
